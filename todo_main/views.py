36796d8cd788a2ed84ae88fe1c7df104eaa78de1
syedaju398@gmail.com <syedaju398@gmail.com>
2025-06-24 11:45:32 +0530

    add-task-function

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from todo_main.models import Task
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') #filter is used fetch thr multiole date 
+    # print(tasks)
+    context = {
+        'tasks': tasks,
+    }
+    return render(request,'home.html' , context)
+
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
